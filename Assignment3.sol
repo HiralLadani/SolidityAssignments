94e632a38261d7c945cfe024a7692b996539fe60
HiralLadani <97958786+HiralLadani@users.noreply.github.com>
2025-05-02 19:26:36 +0530

    Add files via upload
    Swap the Given 2 Numbers Using temporary Number

---
+++ Assignment3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+ contract SwapNumber
+ {
+    
+     function SwapNumbers(int Number1, int Number2)public pure returns (int,int)
+     {
+        int TemNum;
+        TemNum=Number1;
+       Number1 = Number2;
+       Number2  = TemNum;
+       return(Number1,Number2);
+    }
+        
+}
