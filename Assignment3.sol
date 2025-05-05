5322b4ccec797fc0e06f71737ec1e04d7436211d
HiralLadani <97958786+HiralLadani@users.noreply.github.com>
2025-05-05 16:19:18 +0530

    Add files via upload

--- Assignment3.sol
+++ Assignment3.sol
@@ -11,5 +11,12 @@ pragma solidity >=0.7.0 <0.9.0;
        Number2  = TemNum;
        return(Number1,Number2);
     }
+    function SwapNumber1(int Number1, int Number2)public pure returns (int,int)
+     {
+        Number1=Number1+Number2;
+        Number2=Number1-Number2;    
+        Number1=Number1-Number2;
+       return(Number1,Number2);
+    }
         
 }
